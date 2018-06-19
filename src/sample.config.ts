3523b0604d5472445e527f29fb6f6a26c9bcda6a
carrot-e <elenalistratova11@gmail.com>
2018-06-19 23:18:39 +0300

    Authenticate a user and retrieve his reading list

---
+++ src/sample.config.ts
@@ -0,0 +1,10 @@
+export default {
+    apiUrl: 'https://getpocket.com',
+    extensionId: '',
+    consumerKey: '',
+    redirectUri: '',
+    headers: {
+        'Content-Type': 'application/json; charset=UTF8',
+        'X-Accept': 'application/json'
+    }
+};
